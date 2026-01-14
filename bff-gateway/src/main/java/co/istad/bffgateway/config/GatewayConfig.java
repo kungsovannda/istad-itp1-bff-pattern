83da605c60c94fe653fb754b8798acd692ef581f
KungSovannda <kungsovannda@gmail.com>
2026-01-14 16:19:15 +0700

    feat: config gateway with next, product, me

---
+++ bff-gateway/src/main/java/co/istad/bffgateway/config/GatewayConfig.java
@@ -0,0 +1,35 @@
+package co.istad.bffgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("product", r-> r
+                        .path("/api/v1/products")
+                        .uri("http://localhost:8081")
+                )
+                .route("next", r -> r
+                        .path("/**")
+                        .filters(s -> s.stripPrefix(0))
+                        .uri("http://localhost:3000")
+                )
+                .route("next-static", r -> r
+                        .path("/_next/**")
+                        .filters(s -> s.stripPrefix(0))
+                        .uri("http://localhost:3000")
+                )
+                .route("me-route", r->r
+                        .path("/api/v1/me")
+                        .uri("no://op")
+                )
+
+                .build();
+    }
+}
