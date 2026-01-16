3fbdcdc0f3b42ce0e122cd09f4d2ffc9b16489f3
KungSovannda <kungsovannda@gmail.com>
2026-01-16 08:01:08 +0700

    feat: update ui

---
+++ bff-order-service/src/test/java/co/istad/bfforderservice/BffOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.bfforderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
