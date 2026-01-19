bd4f972ab450c30433081ec135f4670bb824795f
KungSovannda <kungsovannda@gmail.com>
2026-01-19 09:52:51 +0700

    update: security in product service

--- bff-product-service/src/main/java/co/istad/bffproductservice/secuity/SecurityConfig.java
+++ bff-product-service/src/main/java/co/istad/bffproductservice/secuity/SecurityConfig.java
@@ -2,6 +2,7 @@ package co.istad.bffproductservice.secuity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +16,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/products").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(auth -> auth
                         .jwt(Customizer.withDefaults())
