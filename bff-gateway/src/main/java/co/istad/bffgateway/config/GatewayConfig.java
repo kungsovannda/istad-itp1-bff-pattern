d70387db15a4ea9eba61fca81379d61067322c84
KungSovannda <kungsovannda@gmail.com>
2026-01-19 09:53:07 +0700

    update: route config in gateway

--- bff-gateway/src/main/java/co/istad/bffgateway/config/GatewayConfig.java
+++ bff-gateway/src/main/java/co/istad/bffgateway/config/GatewayConfig.java
@@ -1,6 +1,7 @@
 package co.istad.bffgateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ public class GatewayConfig {
         return builder.routes()
                 .route("product", r-> r
                         .path("/api/v1/products")
+                        .filters(GatewayFilterSpec::tokenRelay)
                         .uri("http://localhost:8081")
                 )
                 .route("me-route", r->r
