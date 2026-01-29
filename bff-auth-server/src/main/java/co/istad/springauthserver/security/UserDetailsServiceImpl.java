bd45a9069fd20de095e9290a34ae003ef41d7137
KungSovannda <kungsovannda@gmail.com>
2026-01-29 08:53:43 +0700

    feat: implement user details service for custom user details

--- bff-auth-server/src/main/java/co/istad/springauthserver/security/UserDetailsServiceImpl.java
+++ bff-auth-server/src/main/java/co/istad/springauthserver/security/UserDetailsServiceImpl.java
@@ -1,29 +1,40 @@
 package co.istad.springauthserver.security;
 
+import co.istad.springauthserver.domain.Role;
 import co.istad.springauthserver.domain.User;
 import co.istad.springauthserver.feature.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.NullMarked;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Component
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//        org.springframework.security.core.userdetails.User userDetails = new CustomUserDetails();
-//        userDetails.setUser(user);
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .roles(user.getRoles().stream().toString())
-                .password(user.getPassword())
-                .build();
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + username)
+        );
+
+
+        return new CustomUserDetails(
+                user.getUuid(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getFamilyName(),
+                user.getGivenName(),
+                user.isEnabled(),
+                user.getRoles().stream().map(Role::getRole).collect(Collectors.toSet())
+        );
     }
 }
